c21fd2847c9abcece71047a18131cfbc4a788ba2
David Chanfin <david.chanfin@gmail.com>
2026-02-09 17:01:38 +0100

    * added intelephense config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,36 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
-vim.lsp.enable(servers)
+local lspconfig = require "lspconfig"
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- Corrected paths for modern NvChad
+local on_attach = require("nvchad.configs.lspconfig").on_attach
+local on_init = require("nvchad.configs.lspconfig").on_init
+local capabilities = require("nvchad.configs.lspconfig").capabilities
+
+local servers = { "html", "cssls" }
+
+-- Loop through standard servers
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    on_init = on_init,
+    capabilities = capabilities,
+  }
+end
+
+-- Specific configuration for Intelephense
+lspconfig.intelephense.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+  settings = {
+    intelephense = {
+--      environment = {
+--        phpVersion = "5.6.0" 
+--      },
+      files = {
+        maxSize = 5000000; -- Useful for large legacy codebases
+      }
+    }
+  }
+}
