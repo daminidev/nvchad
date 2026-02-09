b903cb311364a1cae3d7aec50d50fdd47532db20
David C <david.chanfin@gmail.com>
2026-02-09 11:22:53 +0100

    Initial custom config

--- lua/options.lua
+++ lua/options.lua
@@ -4,3 +4,39 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+--
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.o.statuscolumn = table.concat({
+  "%s%#Comment#%C",
+  -- Col absolute
+  "%#Comment#%{(v:lnum % 5 == 0) ? v:lnum : ''}",
+  "%#NonText#%{(v:lnum % 5 != 0) ? v:lnum : ''}",
+  -- Separator
+  "%= ",
+  -- Col relative
+  "%#Comment#%{(v:relnum % 5 == 0 && v:relnum != 0) ? v:relnum : ''}",
+  "%#NonText#%{(v:relnum % 5 != 0) ? v:relnum : ''}",
+  "%#NvimCurly#%{(v:relnum == 0 ? '󰜴' : '')}",
+  -- End
+  "%#LineNr#"
+})
+vim.opt.fillchars = {
+  fold = " ",          -- Fill character for the fold line itself
+  foldopen = "",      -- Icon for an expanded fold (requires Nerd Font)
+  foldclose = "",     -- Icon for a collapsed fold
+  foldsep = " ",       -- Column separator for nested folds
+}
+--vim.opt.foldcolumn = "1"
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,17 +12,72 @@ return {
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  {
+    "tpope/vim-fugitive",
+    lazy = false
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    ft = { 'markdown', 'norg', 'rmd', 'org' },
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  --{
+  --  "luukvbaal/statuscol.nvim",
+  --  lazy = false,
+  --  config = function()
+  --    local builtin = require("statuscol.builtin")
+  --    require("statuscol").setup({
+  --      relculright = true, -- relative numbers on the right of absolute
+  --      segments = {
+  --        { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+  --        { text = { "%s" }, click = "v:lua.ScSa" },
+  --        -- Segment 1: Relative
+  --        {
+  --          text = { builtin.lnumfunc, " " }, -- This will show relative distance
+  --          click = "v:lua.ScLa",
+  --        },
+  --        -- Segment 2: Absolute (The fix)
+  --        {
+  --          text = { " %{v:lnum} " }, -- Explicitly request the absolute line number
+  --          color = { fg = "#666666" }, 
+  --          click = "v:lua.ScLa",
+  --        },
+  --      },        
+  --    })
+  --  end,
+  --}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
