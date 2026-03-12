9af74c617722b85cbd078168d6b3fa8568a93aed
David Chanfin <david.chanfin@gmail.com>
2026-03-12 17:31:15 +0100

    * phpcs config for drupal phpcbf phpcs

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,38 @@
 local options = {
+
   formatters_by_ft = {
     lua = { "stylua" },
+    php = { "phpcbf", "php_cs_fixer" },
     -- css = { "prettier" },
     -- html = { "prettier" },
   },
 
+  --
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
   --   timeout_ms = 500,
   --   lsp_fallback = true,
   -- },
+
+   formatters = {
+
+    phpcbf = {
+      -- Disabling stdin is still recommended for phpcbf reliability
+      -- stdin = false, 
+      -- Only run phpcbf if a Drupal/PHPCS config file is detected in the project
+      condition = function(self, ctx)
+        return vim.fs.find({ "phpcs.xml", "phpcs.xml.dist" }, { path = ctx.dirname, upward = true })[1] ~= nil
+      end,
+    },
+
+    php_cs_fixer = {
+      -- Only run php-cs-fixer if a Symfony/API Platform config file is detected
+      condition = function(self, ctx)
+        return vim.fs.find({ ".php-cs-fixer.php", ".php-cs-fixer.dist.php" }, { path = ctx.dirname, upward = true })[1] ~= nil
+      end,
+    },
+  },
+
 }
 
 return options
